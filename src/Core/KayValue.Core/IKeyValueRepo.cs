b5ecd1cb09453053e33ff38a624b045a2ae83353
calebjenkins <caleb@calebjenkins.com>
2023-04-02 10:55:29 -0500

    in memory implementation

---
+++ src/Core/KayValue.Core/IKeyValueRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace Calebs.Data.KeyValues;
+
+public interface IKeyValueRepo
+{
+    T Get<T>(string key) where T : class;
+    void Update<T>(string key, T value) where T : class;
+}
---
+++ src/Core/KayValue.Core/KeyValueInMemory.cs
@@ -0,0 +1,47 @@
+﻿namespace Calebs.Data.KeyValues;
+
+public class KeyValueInMemory : IKeyValueRepo
+{
+    // Using in Memeory JSON formating (string)
+    Dictionary<string, Dictionary<string, string>> _data = new Dictionary<string, Dictionary<string, string>>();
+
+    public T? Get<T>(string key) where T : class
+    {
+        string typeKey = typeof(T).ToString();
+
+        if (_data.ContainsKey(typeKey))
+        {
+            if(_data[typeKey].ContainsKey(key))
+            {
+                T data = _data[typeKey][key].FromJson<T>();
+                return data;
+            }
+        }
+
+        return null;
+    }
+
+    public void Update<T>(string key, T value) where T : class
+    {
+        string typeKey = typeof(T).ToString();
+
+        // Create type dictionary if does now exist
+        if (!_data.ContainsKey(typeKey))
+        {
+            var newDict = new Dictionary<string, string>();
+            newDict.Add(key, value.ToJson());
+
+            _data.Add(typeKey, newDict);
+        }
+
+        // upsert value - create if does not exist, update if does
+        if (!_data[typeKey].ContainsKey(key))
+        {
+            _data[typeKey].Add(key, value.ToJson());
+        }
+        else
+        {
+            _data[typeKey][key] = value.ToJson();
+        }
+    }
+}
