675b193a0e1c1f5d911a8434f272cffe1ec364a1
calebjenkins <caleb@calebjenkins.com>
2023-04-23 16:54:26 -0500

    replace extension class with extension nuget package

--- src/Core/KeyValue.Core/IKeyValueRepo.cs
+++ src/Core/KeyValue.Core/IKeyValueRepo.cs
@@ -3,7 +3,9 @@
 public interface IKeyValueRepo
 {
     T? Get<T>(string key) where T : class;
+    T? Get<T>(int key) where T : class => Get<T>(key.ToString());
     IList<T>? GetAll<T>() where T : class;
 
     void Update<T>(string key, T value) where T : class;
+    void Update<T>(int key, T value) where T : class => Update<T>(key.ToString(), value);
 }
--- src/Core/KeyValue.Core/Usings.cs
+++ src/Core/KeyValue.Core/Usings.cs
@@ -1,1 +1,1 @@
-﻿
+﻿global using Calebs.Extensions;
