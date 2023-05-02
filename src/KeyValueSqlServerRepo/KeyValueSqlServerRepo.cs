eedc2c0b32b6f8b2533cdb5744491fcf423f2b88
calebjenkins <caleb@calebjenkins.com>
2023-05-01 22:34:51 -0500

    adding DB.mdf for SqlRepo Integration tests

--- src/KeyValueSqlServerRepo/KeyValueSqlServerRepo.cs
+++ src/KeyValueSqlServerRepo/KeyValueSqlServerRepo.cs
@@ -1,10 +1,30 @@
-﻿using System.Data.SqlClient;
+﻿using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
 
 namespace Calebs.Data.KeyValueRepo.SqlServer;
 
 public class KeyValueSqlServerRepo : IKeyValueRepo
 {
-    //private var _db = new SqlClient();
+    private string _connString;
+    SqlConnection _conn;
+    ILogger _logger; 
+
+    public KeyValueSqlServerRepo(string connString, ILogger logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _connString = connString ?? throw new ArgumentNullException(nameof(connString));
+
+        _logger.LogDebug("About to open SQL Connection");
+        _conn = new SqlConnection(_connString);
+        _conn.Open();
+
+        _logger.LogDebug("Connection is open");
+
+        _conn.Close();
+
+        _logger.LogDebug("SqlConnection is closed");
+        
+    }
 
     public T? Get<T>(string key) where T : class
     {
--- src/KeyValueTests/SqlServerTests.cs
+++ src/KeyValueTests/SqlServerTests.cs
@@ -1,11 +1,32 @@
-﻿using Calebs.Data.KeyValueRepo.SqlServer;
+﻿using Microsoft.Extensions.Logging;
+using Calebs.Data.KeyValueRepo.SqlServer;
+using Castle.Core.Logging;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+using Moq;
 
 namespace Tests.InMemory;
 
 public class SqlServerTests : InMemoryTests
 {
+    ILogger _logger;
+    string _connString = "";
+
+    public SqlServerTests()
+    {
+        _logger = new Mock<ILogger>().Object;
+        var filePath = System.IO.Path.GetFullPath("../../../Data/DB.mdf");
+        _connString = $"Server=.\\SQLExpress;AttachDbFilename={filePath};Database=db;Trusted_Connection=Yes;";
+    }
+
     public override IKeyValueRepo GetNewInstanceOfRepoForTests()
     {
-        return new KeyValueSqlServerRepo();
+        return new KeyValueSqlServerRepo(_connString, _logger);
+    }
+
+    [Fact]
+    public void CanInitalizeSqlRepoForTests()
+    {
+        var repo = GetNewInstanceOfRepoForTests();
+        repo.Should().NotBeNull();
     }
 }
