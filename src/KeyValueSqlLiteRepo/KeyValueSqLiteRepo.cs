10d2a70098f79a3d7834d81ac4bf61c7dbab5b92
calebjenkins <caleb@calebjenkins.com>
2023-05-28 11:00:31 -0500

    WIP: working on queries

--- src/KeyValueSqlLiteRepo/KeyValueSqLiteRepo.cs
+++ src/KeyValueSqlLiteRepo/KeyValueSqLiteRepo.cs
@@ -1,4 +1,8 @@
 ﻿
+using System;
+using System.Data.Common;
+using System.Diagnostics;
+
 namespace Calebs.Data.KeyValueRepo.SqlLite;
 
 public class KeyValueSqLiteRepo : IKeyValueRepo
@@ -78,5 +82,65 @@ public class KeyValueSqLiteRepo : IKeyValueRepo
     public Task Update<T>(string key, T value) where T : class
     {
         throw new NotImplementedException();
+    }
+
+    private async Task<bool> insertRecord(string key, string valueType, string value, string user)
+    {
+        return false;
+    }
+    private async Task<bool> selectRecord(string key, string valueType)
+    { 
+        // Only one table ever used - right now
+        var tableName = _options.DefaultTableName;
+
+        var sql = "";
+  
+        try
+        {
+            _db.ConfirmOpen();
+            using var connection = _db;
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = sql;
+            command.Parameters.AddWithValue("$table_name", tableName);
+
+            using var reader = await command.ExecuteReaderAsync();
+            while (reader.Read())
+            {
+                var name = reader.GetString(0);
+               
+            }
+            return false;
+        }
+        catch (SqliteException ex)
+        {
+            _logger.LogError("Error validating Default Table: {0}", ex.Message);
+            throw;
+        }
+    }
+
+    private SqliteCommand getCommandForSelect(string key, string valueType, SqliteConnection Db, KeyValueSqlLiteOptions Opt)
+    {
+        var cmd = Db.CreateCommand();
+        cmd.Parameters.AddWithValue("$Key_Column", Opt.ColumnPrefix + Opt.KeyColumnName);
+        cmd.Parameters.AddWithValue("$Key_Value", key);
+
+        var sql = $@"SELECT $Key_Column,
+                            {Opt.ColumnPrefix + Opt.TypeValueColumnName},
+                            {Opt.ColumnPrefix + Opt.ValueColumnName},
+                            {Opt.ColumnPrefix + Opt.CreateByColumnName},
+                            {Opt.ColumnPrefix + Opt.CreateOnColumnName},
+                            {Opt.ColumnPrefix + Opt.UpdatedByColumnName},
+                            {Opt.ColumnPrefix + Opt.UpdatedOnColumnName}
+
+                    FROM $table_name;
+
+                    WHERE $Key_Column = $Key_Value
+                      AND {_options.ColumnPrefix + _options.TypeValueColumnName} = {valueType}
+                      AND {_options.ColumnPrefix + _options.UpdatedOnColumnName} = (SELECT MAX({_options.ColumnPrefix + _options.UpdatedOnColumnName}) FROM yourTable WHERE orderNo = [data].orderNo));";
+
+        return cmd;
+
     }
 }
