31af1c2a08cb4947548a03ebbd20fd4deeb36c17
calebjenkins <caleb@calebjenkins.com>
2023-06-09 20:30:13 -0500

    adjusting path for benchmark.yml

--- RunBenchmarks.cmd
+++ RunBenchmarks.cmd
@@ -1,3 +1,5 @@
 dotnet build -c Release
-.\src\KeyValueRepo.Benchmarks\bin\Release\net7.0\KeyValueRepo.Benchmarks.exe --exporters json --filter *
+# .\src\KeyValueRepo.Benchmarks\bin\Release\net7.0\KeyValueRepo.Benchmarks.exe --exporters json --filter *
+dotnet ./src/KeyValueRepo.Benchmarks/bin/Release/net7.0/KeyValueRepo.Benchmarks.dll --exporters json --filter "KeyValueRepo.Benchmarks.InMemoryBenchmarks.*"
+dotnet ./src/KeyValueRepo.Benchmarks/bin/Release/net7.0/KeyValueRepo.Benchmarks.dll --exporters json --filter "KeyValueRepo.Benchmarks.SQLiteBenchmarks.*"
 pause
